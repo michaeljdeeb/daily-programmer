a3b3fa22c114ab31380f068d0b5badc482782c60
Michael Deeb <michaeldeeb@me.com>
2014-05-07 22:36:14 -0500

    Initial part of the challenge working

--- Week 161/blackjack.rb
+++ Week 161/blackjack.rb
@@ -34,7 +34,8 @@ end
 total_cards = multi_deck.length
 
 total_blackjacks = 0
-total_hands = 1
+# blackjack_hands = Array.new
+total_hands = 0
 while multi_deck.length != 0
     # Select two cards
     card_one_index = rand(1..multi_deck.length) - 1
@@ -47,9 +48,27 @@ while multi_deck.length != 0
     multi_deck.delete_at(card_two_index)
     multi_deck.compact
 
-    #  Check if blackjack!
-    
+    # Check if blackjack!
+    card_one_value = 0
+    card_one[0] > 10 ? card_one_value = 10 : card_one_value = card_one[0]
+    card_one_value = 11 if card_one_value == 1
 
-    puts "Hand ##{total_hands} | Card 1: #{card_one} Card 2: #{card_two} | Array Length: #{multi_deck.length}"
+    card_two_value = 0
+    card_two[0] > 10 ? card_two_value = 10 : card_two_value = card_two[0]
+    card_two_value = 11 if card_two_value == 1
+
+    # Dealing with aces
+    card_two_value = 1 if card_one_value == 1 and card_two_value == 1
+
+    hand_value = card_one_value + card_two_value
+    # BLACKJACK!
+    if hand_value == 21
+        total_blackjacks += 1
+    end
+
+    puts "Hand ##{total_hands} | Card 1: #{card_one} Card 2: #{card_two} Value: #{card_one_value + card_two_value}| Array Length: #{multi_deck.length}"
     total_hands += 1
 end
+blackjack_percent = (total_blackjacks / total_hands.to_f) * 100.0
+blackjack_percent = "%.2f" % blackjack_percent
+puts "After #{total_hands} hands there were #{total_blackjacks} at #{blackjack_percent}%"
